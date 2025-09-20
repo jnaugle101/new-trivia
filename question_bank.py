08ed4689730ada5e13e90700eab5a9fa1bad3225
JeremyN <jeremy_naugle@yahoo.com>
2025-09-20 19:58:55 -0400

    Trivia questions added

--- question_bank.py
+++ question_bank.py
@@ -38,6 +38,15 @@ QUESTIONS = [
     {"q": "What Netflix show features the Upside Down?", "a": "Stranger Things", "category": "Pop Culture"},
     {"q": "Finish the brand: Ben & ____", "a": "Jerry's", "category": "Pop Culture"},
     {"q": "What 2016 mobile game had players catching creatures in AR?", "a": "Pokémon Go", "category": "Pop Culture"},
+    {"q": "Which Kitchen Nightmares celebrity chef is also one of the judges on MasterChef?", "a": "Gordon Ramsay", "category": "Pop Culture"},
+    {"q": "What candy brand encourages people to taste the rainbow?”, "a": "Skittles", "category": "Pop Culture"},
+    {"q": "Restaurants serving up bibimbap, tteokbokki, and japchae have spread all over the world, but where do these dishes originate?", "a": "Korea", "category": "Pop Culture"},
+    {"q": "Who is the author of the cookbook Salt, Fat, Acid, Heat?", "a": "Samin Nosrat", "category": "Pop Culture"},
+    {"q": "What cooking method involves submerging food in a water bath at a precise temperature?", "a": "Sous vide", "category": "Pop Culture"},
+    {"q": "Who is known as the Barefoot Contessa?", "a": "Ina Garten", "category": "Pop Culture"},
+    {"q": "What type of cuisine is the focus of the show The Great British Bake Off?", "a": "British baking","category": "Pop Culture"},
+    {"q": "Which chef is known for the catchphrase Bam!?”, "a": "Emeril Lagasse", "category": "Pop Culture"},
+    {"q": " Who is the celebrity chef known for the cooking show 30-Minute Meals?", "a": "Rachael Ray", "category": "Pop Culture"},
     # optional image question
     #{"q": "Name this artist", "a": "Beyoncé", "category": "Pop Culture", "image": "https://.../beyonce.jpg"},
 
